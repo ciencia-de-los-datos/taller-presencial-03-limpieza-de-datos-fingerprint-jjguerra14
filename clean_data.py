910fd3ee0c54571fed6e4bf859a90193136c8a8e
jguerrab <jguerrab@unal.edu.co>
2024-03-08 18:55:32 -0500

    Intento 1

--- clean_data.py
+++ clean_data.py
@@ -5,11 +5,40 @@ import pandas as pd
 
 
 def load_data(input_file):
-    """Lea el archivo usando pandas y devuelva un DataFrame"""
 
+    # file=open("input.txt", mode="r")
+    # df=pd.DataFrame(file)
+    # file.close()
+    # return(print(df))
+    df=pd.read_csv(input_file)
+    return df
+    """Lea el archivo usando pandas y devuelva un DataFrame"""
+#print(load_data("input.txt"))
 
 def create_fingerprint(df):
+
     """Cree una nueva columna en el DataFrame que contenga el fingerprint de la columna 'text'"""
+
+
+    df = df.copy()
+    df["key"]=df["text"]
+    df["key"]=df["key"].str.strip() # me borra espacios en blanco inicio-final
+    df["key"]=df["key"].str.lower() # Reemplazar todo por minuscula
+    df["key"]=df["key"].str.replace("_","") # Reemplazar todo por minuscula
+    df["key"]=df["key"].str.lower() # Reemplazar todo por minuscula
+    df["key"] = df["key"].str.translate(
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")) # revisar notas al respecto
+    df["key"]=df["key"].str.split()
+    stemmer = nltk.PorterStemmer()
+    df["key"] = df["key"].apply(lambda x: [stemmer.stem(word) for word in x])
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))  # set un conjunto que no se repiten elementos
+    # sorted ordena 
+    df["key"]=df["key"].str.join(" ") # me uno los dos strings de la lista.
+
+    # Toma las palabras individuales y devuelve la raiz.
+    return df
+
+
 
     # 1. Copie la columna 'text' a la columna 'fingerprint'
     # 2. Remueva los espacios en blanco al principio y al final de la cadena
@@ -21,11 +50,22 @@ def create_fingerprint(df):
     # 8. Ordene la lista de tokens y remueve duplicados
     # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
 
-
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
-
     df = df.copy()
+    df = df.sort_values(by=["key", "text"], ascending   =[True, True])
+    keys = df.drop_duplicates(subset="key", keep="first")
+    key_dict=dict(zip(keys["key"], keys["text"]))
+    df["cleaned"] = df["key"].map(key_dict)
+
+    # zip es un iterador :
+    return df
+
+
+
+
+
+  #  df = df.copy()
 
     # 1. Ordene el dataframe por 'fingerprint' y 'text'
     # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
@@ -37,6 +77,19 @@ def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
     # Solo contiene una columna llamada 'texto' al igual
     # que en el archivo original pero con los datos limpios
+    df = df.copy()
+    df = df[["cleaned"]]
+    df = df.rename(columns={"cleaned": "text"})
+    df.to_csv(output_file, index=False)
+    
+
+
+
+df=load_data("input.txt")
+df=create_fingerprint(df)
+df=generate_cleaned_column(df)
+print(df)
+
 
 
 def main(input_file, output_file):
@@ -50,7 +103,7 @@ def main(input_file, output_file):
 
 
 if __name__ == "__main__":
-    main(
-        input_file="input.txt",
-        output_file="output.txt",
-    )
+     main(
+         input_file="input.txt",
+         output_file="output.txt",
+     )
